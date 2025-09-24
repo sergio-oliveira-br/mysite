f575cfa4783d7334256fcd24ba497b9575bdb179
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-24 21:41:12 +0100

    Update settings.py
    act:
    -> debug toolbar added (midedleware)

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'mysite.urls'
