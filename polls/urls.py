f94d09c5a27447fa822842b8b588122b5a0b615d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-25 10:57:19 +0100

    Update urls.py
    act:
    -> endpoint added to router

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello/', views.hello, name='hello'),
+    path('save-question/', views.save_question, name='save-question')
+
 ]
