81e9149ced1a357766d16a2cdd3470768d964607
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-25 14:06:11 +0100

    Update views.py
    act:
    -> method to read date included  in index module (CRUD)

--- polls/views.py
+++ polls/views.py
@@ -8,7 +8,8 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    display_question = Question.objects.all()
+    return render(request, 'index.html', {'display_question': display_question} )
 
 def save_question(request):
     question = Question(question_text=request.POST['question_text'],)
