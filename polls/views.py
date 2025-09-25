82b7ef759ad80723768b1824ed8693b4a09827dd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-25 10:56:49 +0100

    Update views.py
    act:
    -> method crud added

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
+from .models import Question
 
 # Create your views here.
 def index(request):
@@ -7,3 +12,9 @@ def index(request):
 
 def hello(request):
     return render(request, 'index.html', {'name': 'Sergio'})
+
+def save_question(request):
+    question = Question(question_text=request.POST['question_text'],)
+
+    Question.objects.create(question_text = question, pub_date = datetime.now())
+    return redirect('polls:index')
